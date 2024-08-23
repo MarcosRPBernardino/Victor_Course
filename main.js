b84065d8fa274f53ffdbd26824216a455c6fbb65
Marcos Bernardino <marcospivatobernardino@gmail.com>
2024-08-23 01:23:53 +0100

    second commit

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 // Escreva um codigo usando IF que verifica se um número é positivo ou negativo ou zero e mostra uma mensagem no console.log() dizendo se e positivo ou negativo.
 const num = -1
 
-if( num >= 1 ){
+if (num >= 1) {
     console.log('positivo');
-}else if (num === 0) {
-    console.log ('É igual a zero.')
+} else if (num === 0) {
+    console.log('É igual a zero.')
 } else {
     console.log('É negativo.')
 }
 
-const result1 =  num > 0 ? "positivo" : "negativo ou zero";
+const result1 = num > 0 ? "positivo" : "negativo ou zero";
 console.log(result1);
 
-const result2 =  num > 0 ? "positivo" : num === 0 ? "zero" : "negativo";
+const result2 = num > 0 ? "positivo" : num === 0 ? "zero" : "negativo";
 console.log(result2);
 
 // Crie um array com 3 categorias: free, premium e plus e execute os seguintes procedimentos, na ordem a seguir:
@@ -32,8 +32,8 @@ console.log(categorias);
 const real = 1
 const euro = 5
 
-function conversor(){
-    const resultado = real*euro
+function conversor() {
+    const resultado = real * euro
     return (resultado)
 };
 
@@ -44,7 +44,7 @@ console.log(conversor());
 const x = 1
 const y = 5
 
-function conversor(x , y){
+function conversor(x, y) {
     return x * y
 };
 
@@ -54,19 +54,19 @@ const people = {
     name: "Marcos",
     surname: "Bernardino",
     age: 36,
-    talk: function speak(){
+    talk: function speak() {
         console.log('oi');
     }
 }
 
-console.log(people.surname);   
-console.log(people["surname"]); 
+console.log(people.surname);
+console.log(people["surname"]);
 console.log(people.talk);
 console.log(people.talk());
 
 // Criar uma funcao que some dois numeros e retorne o valor somado para a variavel "sum"
 
-function soma(x2, y2){
+function soma(x2, y2) {
     return x2 + y2
 }
 
@@ -118,7 +118,7 @@ console.log(soma(14, 8));
 // Posicional
 
 function fullName(nome, nomeDomeio, sobrenome) {
-    return nome +' '+ nomeDomeio +' '+ sobrenome;
+    return nome + ' ' + nomeDomeio + ' ' + sobrenome;
 }
 
 const nomeCompleto = fullName('Pedro', 'Henrique', 'Bernardino');
@@ -127,11 +127,11 @@ console.log(nomeCompleto);
 
 // Não posicional
 
-function concNome ({nome, nomeDoMeio, sobrenome}){
-    return nome +' '+ nomeDoMeio +' '+ sobrenome;
+function concNome({ nome, nomeDoMeio, sobrenome }) {
+    return nome + ' ' + nomeDoMeio + ' ' + sobrenome;
 }
 
-const nomeCompleto2 = concNome({sobrenome: 'Pivato', nome: 'Peter', nomeDoMeio: 'Potter'});
+const nomeCompleto2 = concNome({ sobrenome: 'Pivato', nome: 'Peter', nomeDoMeio: 'Potter' });
 
 console.log(nomeCompleto2);
 
@@ -164,7 +164,7 @@ console.log(nomeCompleto2);
 // Obs.: utilize parametros nao posicionais.
 
 
-function verificarMaioridade({idade}){
+function verificarMaioridade({ idade }) {
     return idade >= 18
 }
 
@@ -173,10 +173,10 @@ const age2 = 15;
 const age3 = 12;
 const age4 = 40;
 
-console.log(verificarMaioridade({idade : age}));
-console.log(verificarMaioridade({idade : age2}));
-console.log(verificarMaioridade({idade : age3}));
-console.log(verificarMaioridade({idade : age4}));
+console.log(verificarMaioridade({ idade: age }));
+console.log(verificarMaioridade({ idade: age2 }));
+console.log(verificarMaioridade({ idade: age3 }));
+console.log(verificarMaioridade({ idade: age4 }));
 
 
 // Corrija a função
@@ -202,8 +202,8 @@ function isVogal(caractere) {
 }
 
 // Teste
-console.log(isVogal('a')); 
-console.log(isVogal('b')); 
+console.log(isVogal('a'));
+console.log(isVogal('b'));
 
 // - Crie um array com 5 times de futebol ou frutas e execute os seguintes procedimentos, na ordem a seguir:
 // 	- Acesse o elemento de índice/posição 3.
@@ -213,7 +213,7 @@ console.log(isVogal('b'));
 // 	- Delete o último elemento do array.
 // 	- Obs.: Exibir o resultado no console da cada um dos codigos executado acima.
 
-const frutas= ['banana', 'maçã', 'uva', 'pera', 'pessego'];
+const frutas = ['banana', 'maçã', 'uva', 'pera', 'pessego'];
 
 console.log(frutas[3]);
 
@@ -237,7 +237,7 @@ console.log(frutas);
 
 const numbers = [1, 2, 3, 4];
 
-const multipliedNumbers = numbers.map(item =>  item * 2);
+const multipliedNumbers = numbers.map(item => item * 2);
 
 console.log(multipliedNumbers);
 
@@ -254,7 +254,7 @@ console.log(multSoma);
 const base = 20
 const altura = 40
 
-const areaTriangle = (x, y) => (x * y)/2 ;
+const areaTriangle = (x, y) => (x * y) / 2;
 
 console.log(areaTriangle(base, altura));
 
@@ -299,7 +299,7 @@ console.log(user.type);
 // 	- Delete o último elemento do array.
 // 	- Exiba o resultado no console de cada um dos códigos executados acima.
 
-const citys = ['São Paulo', 'Rio de Janeiro','Salvador','Bauru', 'Florianópolis'];
+const citys = ['São Paulo', 'Rio de Janeiro', 'Salvador', 'Bauru', 'Florianópolis'];
 
 console.log(citys[3]);
 
@@ -323,10 +323,10 @@ console.log(citys);
 
 const countries = ["Brasil", "Irlanda", "China"];
 
-    const upperCaseCountries = countries.map(country => country.toUpperCase())
+const upperCaseCountries = countries.map(country => country.toUpperCase())
 
-    console.log(upperCaseCountries);
-    
+console.log(upperCaseCountries);
+
 //     Criar um objeto "animal" com as seguintes propriedades:
 // - nome = Mingau
 // - sobrenome = Shiro
@@ -342,19 +342,19 @@ const animal = {
     nome: 'Mingau',
     sobrenome: 'Shiro',
     idade: '6',
-    miar: function(){
+    miar: function () {
         console.log('miau!');
-        
-    } 
+
+    }
 }
 
 console.log(animal['nome']);
 
 console.log(animal.idade);
 
-function descricao(){
-    console.log('O nome do animal é'+ ' ' + animal.nome + ' ' + animal.sobrenome + ' ' + 'e ele tem atualmente' + ' ' + animal.idade + ' ' + 'anos.');
-    
+function descricao() {
+    console.log('O nome do animal é' + ' ' + animal.nome + ' ' + animal.sobrenome + ' ' + 'e ele tem atualmente' + ' ' + animal.idade + ' ' + 'anos.');
+
 }
 
 descricao();
@@ -362,11 +362,94 @@ descricao();
 // Dado o array de objeto "people" abaixo, utilizer o array map para retornar em uma variavel "result" o nome de cada pessoa.
 
 const pessoas = [
-	{ name: 'Alice' },
-	{ name: 'Bob' },
-	{ name: 'Charlie' },
+    { name: 'Alice' },
+    { name: 'Bob' },
+    { name: 'Charlie' },
 ];
 
 const result = pessoas.map(person => person.name);
 
 console.log(result);
+
+// Escreva um programa que leia a idade de uma pessoa e verifique se ela pode votar (ou seja, maior de 16 anos, no caso do Brasil). Exiba "Você pode votar" ou "Você não pode votar".
+
+const idadeVoto = 14;
+
+const validacao = idadeVoto >= 16 ? true : false;
+console.log(validacao);
+
+// Criar um switch para mostrar qual e o tipo de assinatura do usuario. Conforme os procedimentos abaixo:
+// 	- Criar uma variavel "assinatura" com o valor "free"
+// 	- Criar um Switch com 3 opcoes (free, premium e plus)
+// 	- Exibir no console.log() o valor de "assinatura". Ou seja, ao mudar o valor da variavel "assinatura" o switch deve mudar o valor exibido no console.log()
+
+
+const assinatura = 'premium';
+
+switch (assinatura) {
+    case 'free':
+        console.log('Sua assinatura e free.');
+        break;
+    case 'premium':
+        console.log('Sua assinatura e premium');
+        break;
+    case 'plus':
+        console.log('Sua assinatura e plus.');
+        break;
+    default:
+        console.log('Nao e possivel conferir sua assinatura.');
+        break;
+}
+
+// Considere uma função evaluateGrade que recebe uma letra representando uma nota (A, B, C, D, F) e retorna uma mensagem apropriada (por exemplo, "Excelente" para A, "Bom" para B, etc.). Implemente essa função usando switch.
+
+const evaluateGrade = 'F';
+
+switch (evaluateGrade) {
+    case 'A':
+        console.log('Excelente');
+        break;
+    case 'B':
+        console.log('Bom trabalho');
+        break;
+    case 'C':
+        console.log('Na média');
+        break;
+    case 'D':
+        console.log('Não foi o suficiente');
+        break;
+    case 'F':
+        console.log('Estude mais');
+        break;
+    default: console.log('Impossivel encontrar sua avaliacao');
+        break;
+}
+
+// Crie um objeto chamado livro com as propriedades titulo ("Harry Potter"), autor (J.K. Rowling) e ano (2000). Em seguida, crie uma função chamada detalhesDoLivro que retorna uma string com todos os detalhes do livro.
+
+const livro = {
+    titulo: 'Harry Potter',
+    autor: 'J.K. Rowling',
+    ano: '2000',
+}
+
+function detalhesDoLivro() {
+    return `O nome do livro é ${livro.titulo}, da autora ${livro.autor} e foi lançado no ano  ${livro.ano}.`;
+}
+
+console.log(detalhesDoLivro());
+
+// Dado o array de objetos abaixo, faça uma função que retorne um array com apenas as notas de cada estudantes.
+
+
+const students = [
+    { name: 'Quincy', grade: 96 },
+    { name: 'Jason', grade: 84 },
+    { name: 'Alexis', grade: 100 },
+    { name: 'Sam', grade: 65 },
+    { name: 'Katie', grade: 90 }
+];
+
+const studentNotes = students.map ( nota => nota.grade);
+
+console.log(studentNotes);
